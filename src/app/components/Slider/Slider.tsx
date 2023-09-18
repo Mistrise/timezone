f7be117d26ec54337c47d7325b39cd70b7b2ca21
Aleksey Ivanov <ale.ivanov@corp.mail.ru>
2023-09-18 18:16:26 +0300

    adding CardScheduler.tsx

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -2,13 +2,46 @@ import styles from './Slider.module.css'
 import Image from "next/image";
 import Dot from '../../../../public/icons/State=Off.svg'
 import SliderTimer from "@/app/components/SliderTimer/SliderTimer";
+import {useRef, useState} from "react";
 
 const Slider = () => {
-    const timer = true
+    let startX: number | null = null
+    let innerScrollElement = useRef(null);
+    let [timeElement, setTimeElement] = useState('');
+    let totalDiffX = 0;
+    let currentDiffX = 0;
+
+    const PIXELS_PER_HOUR = 50;
+
+    console.log(timeElement)
+    function calcRoundedTime(pxOffset: number) {
+        const hoursOffset = Math.round(pxOffset / PIXELS_PER_HOUR * 2) / 2;
+        if (hoursOffset === 0) {
+            setTimeElement('');
+        } else {
+            const sign = hoursOffset > 0 ? '+' : '';
+            setTimeElement(`${sign}${hoursOffset}h`);
+        }
+    }
+
     return (
-        <div className={styles.container}>
-            {timer ? <SliderTimer/> : <Image src={Dot} alt={''}></Image>}
-            <div className={styles.slider__background}></div>
+        <div className={styles.container}
+             onMouseDown={event => startX = event.clientX}
+             onMouseUp={event => {
+                 totalDiffX = currentDiffX;
+                 startX = null;
+             }}
+             onMouseMove={event => {
+                 if (startX) {
+                     const diffX = event.clientX - startX;
+                     currentDiffX = totalDiffX - diffX;
+                     innerScrollElement.current.style.transform = `translate(${diffX}px)`;
+                     calcRoundedTime(currentDiffX)
+                 }
+             }}
+        >
+            {timeElement !== '' ? <SliderTimer time={timeElement} setTimeElement={setTimeElement}/> : <Image src={Dot} alt={''}></Image>}
+            <div className={styles.slider__background} ref={innerScrollElement}></div>
         </div>
     )
 }
--- src/app/components/SliderTimer/SliderTimer.tsx
+++ src/app/components/SliderTimer/SliderTimer.tsx
@@ -2,11 +2,17 @@ import Cross from '../../../../public/icons/Icon=cross-circle.svg'
 import Image from "next/image";
 import styles from './SliderTimer.module.css'
 
-const SliderTimer = () => {
+interface Props {
+    time: string
+    setTimeElement: (state: string) => void
+}
+
+const SliderTimer = ({time, setTimeElement}: Props) => {
+
     return (
         <div className={styles.slider__timer}>
-            <div className={styles.slider__text}>+30 min</div>
-                <Image src={Cross} alt={''} width={16} height={16}></Image>
+            <div className={styles.slider__text}>{time}</div>
+                <Image src={Cross} alt={''} width={16} height={16} onClick={() => setTimeElement('')}></Image>
         </div>
     )
 }
