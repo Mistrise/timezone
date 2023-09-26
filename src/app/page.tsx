5b2b3093794674682f426ac1840a03b04068b62e
Aleksey Ivanov <ale.ivanov@corp.mail.ru>
2023-09-26 14:21:20 +0300

    small layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Button from "@/app/components/Button/Button";
 import Heading from "@/app/components/Heading/Heading";
 import Slider from "@/app/components/Slider/Slider";
 import Tab from "@/app/components/Tabs/Tab";
-import {useState} from "react";
+import {createContext, useContext, useState} from "react";
 import Card from "@/app/components/Card/Card";
 import Container from "@/app/components/Container/Container";
-import {days, month} from "@/constants/constants";
 import CardScheduler from "@/app/components/CardScheduler/CardScheduler";
+import {citiesConst} from "@/constants/constants";
 
 export interface City {
     id: number
@@ -18,83 +18,37 @@ export interface City {
     minutes: number
     date: string
 }
+const TimeContext = createContext('')
 
 export default function Home() {
 
-    const globalDate = new Date()
+
+
+    const [globalTime, setGlobalTime] = useState<null | string>('')
 
     const [calendarType, setCalendarType] = useState(true)
 
     const [timeFormat, setTimeFormat] = useState(true)
 
-    const [cities, setCity] = useState<City[]>([
-        {
-            id: 1,
-            city: 'Moscow',
-            timezone: `GMT ${
-               globalDate.getTimezoneOffset() < 0 ? 
-                   "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
-                   "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
-            }`,
-            hours: globalDate.getHours(),
-            minutes: globalDate.getMinutes(),
-            date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
-        },
-        {
-            id: 2,
-            city: 'London',
-            timezone: `GMT ${
-               globalDate.getTimezoneOffset() < 0 ? 
-                   "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
-                   "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
-            }`,
-            hours: globalDate.getHours(),
-            minutes: globalDate.getMinutes(),
-            date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
-        },
-        {
-            id: 3,
-            city: 'New York',
-            timezone: `GMT ${
-               globalDate.getTimezoneOffset() < 0 ? 
-                   "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
-                   "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
-            }`,
-            hours: globalDate.getHours(),
-            minutes: globalDate.getMinutes(),
-            date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
-        },
-        {
-            id: 4,
-            city: 'Paris',
-            timezone: `GMT ${
-               globalDate.getTimezoneOffset() < 0 ? 
-                   "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
-                   "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
-            }`,
-            hours: globalDate.getHours(),
-            minutes: globalDate.getMinutes(),
-            date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
-        },
-    ])
+    const [cities, setCity] = useState<City[]>(citiesConst)
 
 
-  return (
-      <>
-          <Heading/>
-          <Container>
-              <Slider/>
-              {calendarType ?
-                  cities.map(city => <Card timeFormat={timeFormat} city={city} key={city.id}/>)
-                  :
-                  cities.map(city => <CardScheduler timeFormat={timeFormat} key={city.id} city={city}/>)
-              }
-              <div style={{display: "flex", flexDirection: "row"}}>
-                  <Button/>
-                  <Tab elem1={'Normal'} elem2={'Scheduler'} prevState={calendarType} setState={setCalendarType}/>
-                  <Tab elem1={'24H'} elem2={'AM/PM'} prevState={timeFormat} setState={setTimeFormat}/>
-              </div>
-          </Container>
-      </>
+    return (
+          <TimeContext.Provider value={globalTime}>
+              <Heading/>
+              <Container>
+                  <Slider/>
+                  {calendarType ?
+                      cities.filter(city => city.id < 4).map(city => <Card timeFormat={timeFormat} city={city} key={city.id}/>)
+                      :
+                      cities.filter(city => city.id < 4).map(city => <CardScheduler timeFormat={timeFormat} key={city.id} city={city}/>)
+                  }
+                  <div style={{display: "flex", flexDirection: "row"}}>
+                      <Button/>
+                      <Tab elem1={'Normal'} elem2={'Scheduler'} prevState={calendarType} setState={setCalendarType}/>
+                      <Tab elem1={'24H'} elem2={'AM/PM'} prevState={timeFormat} setState={setTimeFormat}/>
+                  </div>
+              </Container>
+          </TimeContext.Provider>
   )
 }
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
+import {City} from "@/app/page";
+
 export const days = [
     'Sun',
     'Mon',
@@ -10,4 +12,69 @@ export const days = [
 
 export const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+]
+
+export const globalDate = new Date()
+
+export const citiesConst: City[] = [
+    {
+        id: 1,
+        city: 'Moscow',
+        timezone: `GMT ${
+            globalDate.getTimezoneOffset() < 0 ?
+                "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
+                "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
+        }`,
+        hours: globalDate.getHours(),
+        minutes: globalDate.getMinutes(),
+        date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
+    },
+    {
+        id: 2,
+        city: 'London',
+        timezone: `GMT ${
+            globalDate.getTimezoneOffset() < 0 ?
+                "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
+                "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
+        }`,
+        hours: globalDate.getHours(),
+        minutes: globalDate.getMinutes(),
+        date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
+    },
+    {
+        id: 3,
+        city: 'New York',
+        timezone: `GMT ${
+            globalDate.getTimezoneOffset() < 0 ?
+                "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
+                "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
+        }`,
+        hours: globalDate.getHours(),
+        minutes: globalDate.getMinutes(),
+        date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
+    },
+    {
+        id: 4,
+        city: 'Paris',
+        timezone: `GMT ${
+            globalDate.getTimezoneOffset() < 0 ?
+                "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
+                "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
+        }`,
+        hours: globalDate.getHours(),
+        minutes: globalDate.getMinutes(),
+        date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
+    },
+    {
+        id: 5,
+        city: 'Washington',
+        timezone: `GMT ${
+            globalDate.getTimezoneOffset() < 0 ?
+                "+ " + (Math.abs(globalDate.getTimezoneOffset() / 60)) :
+                "- " + (Math.abs(globalDate.getTimezoneOffset() / 60))
+        }`,
+        hours: globalDate.getHours(),
+        minutes: globalDate.getMinutes(),
+        date: `${days[globalDate.getDay()]} ${globalDate.getDate()} ${month[globalDate.getMonth()]}`
+    },
 ]
