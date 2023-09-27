94ba78eed3fecfb133f2b82075fecf8757884b2f
Aleksey Ivanov <ale.ivanov@corp.mail.ru>
2023-09-27 18:43:52 +0300

    fix modal

--- src/app/components/CitiesModal/CitiesModal.tsx
+++ src/app/components/CitiesModal/CitiesModal.tsx
@@ -3,19 +3,23 @@ import ModalSearch from "@/app/components/ModalSearch/ModalSearch";
 import ModalList from "@/app/components/ModalList/ModalList";
 import Close from '../../../../public/icons/Icon=Close.svg'
 import Image from "next/image";
+import {useState} from "react";
 
 interface Props {
     setShowSearch: (show: boolean) => void
 }
 
 const CitiesModal = ({setShowSearch}:Props) => {
+    const [modalInput, setModalInput] = useState('')
+
+    console.log(modalInput)
     return (
         <div className={styles.modal__container}>
             <div className={styles.modal__title}>
                 <div className={styles.modal__title__text}>Add City</div>
                 <Image src={Close} alt={''} className={styles.modal__title__image} onClick={() => setShowSearch(false)}/>
             </div>
-            <ModalSearch/>
+            <ModalSearch modalInput={modalInput} setModalInput={setModalInput}/>
             <ModalList/>
         </div>
     )
--- src/app/components/ModalSearch/ModalSearch.tsx
+++ src/app/components/ModalSearch/ModalSearch.tsx
@@ -1,9 +1,20 @@
 import styles from './ModalSearch.module.css'
 
-const ModalSearch = () => {
+interface Props {
+    modalInput: string
+    setModalInput: (input: string) => void
+}
+
+
+const ModalSearch = ({modalInput, setModalInput}: Props) => {
+
   return (
       <form className={styles.modal__search__form}>
-          <input className={styles.modal__search__input} type="text" name={'cities_search_field'} placeholder={'Enter city'}/>
+          <input
+              className={styles.modal__search__input}
+              type="text" name={'cities_search_field'}
+              placeholder={'Enter city'}
+              onChange={(event) => setModalInput(event.target.value) }/>
       </form>
   )
 }
