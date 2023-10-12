1f8611a86c8d9d85a28dd9a21e02aaf7d9996ddf
Aleksey Ivanov <ale.ivanov@corp.mail.ru>
2023-10-12 14:58:22 +0300

    fixing layout

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,8 +1,10 @@
 import styles from './Card.module.css'
 import {City} from "@/app/page";
 import Sunny from '../../../../public/icons/State=Sunny.svg'
+import Night from '../../../../public/icons/State=Clear-night.svg'
 import Image from "next/image";
 import {useTimeStore} from "@/app/store";
+import {days, month} from "@/constants/constants";
 
 interface Props {
     city: City
@@ -40,13 +42,18 @@ const Card = ({city, timeFormat, dragItem, dragOverItem, handleSort, index}: Pro
             {timeFormat ?
                 null :
                 <span className={styles.card__am}>
-                    {city.hours  > 12 ? <p>PM</p> : <p>AM</p>}
+                    {currentDate.getHours()  > 12 ? <p>PM</p> : <p>AM</p>}
                 </span>}
         </div>
         <div className={styles.card__timezone}>GMT {city.timezone < 10 ? `0${city.timezone}:00` : `${city.timezone}:00`}</div>
         <div className={styles.card__date}>
-            <Image src={Sunny} width={18} height={18} alt='' style={{marginRight: '3px'}}></Image>
-            {city.date}
+            {currentDate.getHours() > 7 && currentDate.getHours() < 22 ?
+                <Image src={Sunny} width={18} height={18} alt='' style={{marginRight: '3px'}}></Image>
+                :
+                <Image src={Night} width={18} height={18} alt='' style={{marginRight: '3px'}}></Image>
+            }
+
+            {`${days[currentDate.getDay()]} ${currentDate.getDate()} ${month[currentDate.getMonth()]}`}
         </div>
     </div>
 }
