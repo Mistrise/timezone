e26a8706fa1c4c21b26bfd17951a57495ad46256
Alex <alekseiivanov.2016@inbox.ru>
2024-05-21 12:40:42 +0300

    changing time format "+0.5 h" to "+ 1:30 h"

--- src/app/components/SliderTimer/SliderTimer.tsx
+++ src/app/components/SliderTimer/SliderTimer.tsx
@@ -17,8 +17,8 @@ const SliderTimer = ({onReset} : Props) => {
     <div className={`${styles.slider__timer} ${!hoursOffset && styles.slider__timer_disabledState}`}>
           <div className={styles.slider__text}>
               {hoursOffset > 0 ?
-                  `+${hoursOffset.toString().endsWith('5') ? `${hoursOffset.toFixed(0)}:30` : `${hoursOffset}:00` }h` :
-                  `${hoursOffset.toString().endsWith('5') ? `${hoursOffset.toFixed(0)}:30` : `${hoursOffset}:00` }h`}
+                  `+${hoursOffset.toString().endsWith('5') ? `${Math.trunc(hoursOffset)}:30` : `${hoursOffset}:00` }h` :
+                  `${hoursOffset.toString().endsWith('5') ? `${Math.trunc(hoursOffset)}:30` : `${hoursOffset}:00` }h`}
           </div>
           <Image
             src={Cross} alt={''} width={16} height={16}
