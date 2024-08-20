5314a5c3ef1795062c2ac7edffb197c9281cefd3
alex <alekseiivanov.2016@inbox.ru>
2024-08-20 17:31:00 +0300

    feat: creating blogs structure

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Container from "@/app/components/Container/Container";
+import {BasicText, HeadingH1} from "@/ui/typography";
+import BlogsGrid from "@/app/blog/components/BlogsGrid/BlogsGrid";
 
 const Page = () => {
     return (
         <Container>
-            <h1>Blog</h1>
-            <p>A Time Zone Converter is an intuitive and user-friendly tool designed to help users quickly and accurately convert time across different time zones worldwide. Whether you’re scheduling international meetings, coordinating with global teams, or planning travel, this tool makes it easy to compare local times in various cities and countries.</p>
+            <HeadingH1>Blog</HeadingH1>
+            <BasicText>A Time Zone Converter is an intuitive and user-friendly tool designed to help users quickly and accurately convert time across different time zones worldwide. Whether you’re scheduling international meetings, coordinating with global teams, or planning travel, this tool makes it easy to compare local times in various cities and countries.</BasicText>
+            <BlogsGrid/>
         </Container>
     );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from 'next'
 import { Public_Sans } from 'next/font/google'
 import GoogleAnalyticsScript from "@/app/GoogleAnalyticsScript";
 import NavBar from "@/app/components/NavBar/NavBar";
-
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
 const publicSansFont = Public_Sans({ weight: ['400', '500', '600', '700'], subsets: ['latin'] })
 
@@ -22,6 +24,42 @@ export const metadata: Metadata = {
     description: 'Service that shows time in different cities'
   }
 }
+
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+      fileContents
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    // @ts-ignore
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
 
 export default function RootLayout({
   children,
