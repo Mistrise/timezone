59836304c32b5725493ed43acb5adf7c7a375feb
Granti4ek <155064246+Granti4ek@users.noreply.github.com>
2024-09-14 22:47:20 -0700

    Fixed bottom margin
    • Deleted "Container" from Navbar
    •Added manual props to .navBarContainer

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,36 +1,34 @@
 'use client'
 import styles from './NavBar.module.css'
-import Container from "@/app/components/Container/Container";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import timeHubIcon from '../../apple-icon.png'
 import Image from "next/image";
 
 const NavBar = () => {
-    const pathname = usePathname()
+    const pathname = usePathname();
+    
     return (
-        <Container>
-            <div className={styles.navBarContainer}>
-                <Link href={'/'} className={styles.navLink}>
-                    <div className={`${styles.navBarItem} ${pathname === '/' ? `${styles.active}` : ''}`}>
-                        <Image src={timeHubIcon} alt='logo' width={24} height={24}/>
-                        <p style={{marginLeft: '8px'}}>TimeHub</p>
+        <div className={styles.navBarContainer}>
+            <Link href={'/'} className={styles.navLink}>
+                <div className={`${styles.navBarItem} ${pathname === '/' ? `${styles.active}` : ''}`}>
+                    <Image src={timeHubIcon} alt='logo' width={24} height={24}/>
+                    <p style={{ marginLeft: '8px' }}>TimeHub</p>
+                </div>
+            </Link>
+            <div className={styles.navBarContainerSecondaryItems}>
+                <Link href={'/about'} className={styles.navLink}>
+                    <div className={`${styles.navBarItem} ${pathname === '/about' ? `${styles.active}` : ''}`}>
+                        About
                     </div>
                 </Link>
-                <div className={styles.navBarContainerSecondaryItems}>
-                    <Link href={'/about'} className={styles.navLink}>
-                        <div className={`${styles.navBarItem} ${pathname === '/about' ? `${styles.active}` : ''}`}>
-                            About
-                        </div>
-                    </Link>
-                    <Link href={'/blog'} className={styles.navLink}>
-                        <div className={`${styles.navBarItem} ${pathname.startsWith('/blog') ? `${styles.active}` : ''}`}>
-                            Blog
-                        </div>
-                    </Link>
-                </div>
+                <Link href={'/blog'} className={styles.navLink}>
+                    <div className={`${styles.navBarItem} ${pathname.startsWith('/blog') ? `${styles.active}` : ''}`}>
+                        Blog
+                    </div>
+                </Link>
             </div>
-        </Container>
+        </div>
     );
 };
 
