8d4426e0bc9d696e2138018d3da22bf4767a679b
Granti4ek <155064246+Granti4ek@users.noreply.github.com>
2025-05-06 21:49:26 -0700

    Slider update

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import styles from './Slider.module.css'
-import React, {useCallback, useEffect, useRef} from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import SliderTimer from "@/app/components/SliderTimer/SliderTimer";
-import {useRapidTimeStore, useTimeStore} from "@/app/store";
-
+import { useRapidTimeStore, useTimeStore } from "@/app/store";
 
 const PIXELS_PER_HOUR = 50;
 
@@ -16,16 +15,27 @@ const Slider = () => {
   const setHoursOffset = useTimeStore(state => state.setHoursOffset)
   const setSecondsOffset = useRapidTimeStore(state => state.setSecondsOffset)
 
+  const [bottomPadding, setBottomPadding] = useState(0);
+
   const onReset = () => {
     totalDiffXRef.current = 0;
   }
-
 
   const calcRoundedTime = useCallback((pxOffset: number) => {
     const hoursOffset = Math.round(pxOffset / PIXELS_PER_HOUR * 2) / 2;
     setHoursOffset(hoursOffset)
     setSecondsOffset(Math.round(pxOffset / PIXELS_PER_HOUR * 60 * 60))
   }, [setHoursOffset])
+
+  useEffect(() => {
+    const isiOS = /iPhone/.test(navigator.userAgent);
+    const hasNotch = window.screen.height >= 812;
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
+    
+    if (isiOS && hasNotch && isStandalone) {
+      setBottomPadding(24);
+    }
+  }, []);
 
   useEffect(() => {
     let startX: number | null = null
@@ -40,7 +50,7 @@ const Slider = () => {
       const onMouseMove = (e: MouseEvent) => {
         if (startX) {
           const diffX = e.clientX - startX;
-          currentDiffX =  totalDiffXRef.current - diffX;
+          currentDiffX = totalDiffXRef.current - diffX;
           calcRoundedTime(currentDiffX)
           if (innerScrollElement.current) {
             innerScrollElement.current.style.transform = `translate(${diffX}px)`;
@@ -67,7 +77,7 @@ const Slider = () => {
       const onTouchMove = (e: TouchEvent) => {
         if (startX) {
           const diffX = e.touches[0].clientX - startX;
-          currentDiffX =  totalDiffXRef.current - diffX;
+          currentDiffX = totalDiffXRef.current - diffX;
           calcRoundedTime(currentDiffX)
           if (innerScrollElement.current) {
             innerScrollElement.current.style.transform = `translate(${diffX}px)`;
@@ -93,9 +103,10 @@ const Slider = () => {
   }, [setHoursOffset, containerRef.current]);
 
   return (
-    <div className={styles.container}
-         ref={containerRef}
-    >
+    <div
+      className={styles.container}
+      ref={containerRef}
+      style={{ paddingBottom: `${bottomPadding}px` }}   >
       <SliderTimer onReset={onReset}/>
       <div className={styles.fade__left}></div>
       <div className={styles.fade__right}></div>
