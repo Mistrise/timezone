d59cb736bda532fba5ec8f00b0f6c61eff8f8d85
Alexey Ivanov <lexa995@inbox.ru>
2025-05-10 11:50:18 +0300

    remove sw to fix problem

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,36 @@
-const installEvent = () => {
-    self.addEventListener('install', () => {
-        console.log('service worker installed');
-    });
-};
-installEvent();
-
-const activateEvent = () => {
-    self.addEventListener('activate', () => {
-        console.log('service worker activated');
-    });
-};
-activateEvent();
-
-const cacheName = 'v1'
-
-const cacheClone = async (e) => {
-    const res = await fetch(e.request);
-    const resClone = res.clone();
-    const cache = await caches.open(cacheName);
-    const bigCities = await fetch('https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000000@public/records?')
-    await cache.put(e.request, resClone);
-    return res;
-};
-
-const fetchEvent = () => {
-    self.addEventListener('fetch', (e) => {
-        e.respondWith(
-            cacheClone(e)
-                .catch(() => caches.match(e.request))
-                .then((res) => res)
-        );
-    });
-};
-
-fetchEvent();
+// const installEvent = () => {
+//     self.addEventListener('install', () => {
+//         console.log('service worker installed');
+//     });
+// };
+// installEvent();
+//
+// const activateEvent = () => {
+//     self.addEventListener('activate', () => {
+//         console.log('service worker activated');
+//     });
+// };
+// activateEvent();
+//
+// const cacheName = 'v1'
+//
+// const cacheClone = async (e) => {
+//     const res = await fetch(e.request);
+//     const resClone = res.clone();
+//     const cache = await caches.open(cacheName);
+//     const bigCities = await fetch('https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000000@public/records?')
+//     await cache.put(e.request, resClone);
+//     return res;
+// };
+//
+// const fetchEvent = () => {
+//     self.addEventListener('fetch', (e) => {
+//         e.respondWith(
+//             cacheClone(e)
+//                 .catch(() => caches.match(e.request))
+//                 .then((res) => res)
+//         );
+//     });
+// };
+//
+// fetchEvent();
